f6d688a2a9debbd669fd13b536745bfa5cdc4034
Daniel Kopko <dkopko@runbox.com>
2016-08-29 13:08:33 -0400

    Copyright notices for LGPLv3 and contribs.

--- src/cb.h
+++ src/cb.h
@@ -1,3 +1,19 @@
+/* Copyright 2016 Daniel Kopko */
+/*
+ * This file is part of CB.
+ *
+ * CB is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software
+ * Foundation, version 3 of the License.
+ *
+ * CB is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with CB.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #ifndef _CB_H_
 #define _CB_H_
 
