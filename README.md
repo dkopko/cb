1c63c2c9d8b34e67f53f7d7f33642b9c2c0707c7
Daniel Kopko <dkopko@runbox.com>
2016-08-30 11:20:51 -0400

    Made single entry point for testing.

--- README.md
+++ README.md
@@ -12,19 +12,16 @@ make
 ## Test Coverage
 
 ```
-lcov --directory . --zerocounters --rc lcov_branch_coverage=1
+./scripts/test_suite.sh
 ```
 
-Now run your tests.
-
+To automatically display the results in your preferred browser:
 ```
-lcov --directory . --capture --output-file coverage.info --rc lcov_branch_coverage=1
-lcov --remove coverage.info '/usr/*' --output-file coverage.info.cleaned --rc lcov_branch_coverage=1
-genhtml -o coverage coverage.info.cleaned --rc lcov_branch_coverage=1
-rm coverage.info coverage.info.cleaned
+./scripts/test_suite.sh -s 
 ```
 
-Test coverage is in coverage/index.html.
+The latest report will be in `TESTRUNS/latest_success/index.html`.
+
 
 ## Contact
 
---
+++ scripts/test_suite.sh
@@ -0,0 +1,155 @@
+#!/usr/bin/env bash
+# Copyright 2016 Daniel Kopko.
+
+set -o nounset
+
+PROJECT_ROOT="$(cd "$(dirname "$0")/.." ; pwd)"
+
+#Overrideable settings
+BUILD_ROOT="${BUILD_ROOT:-${PROJECT_ROOT}/BUILD}"
+TESTRUNS_ROOT="${TESTRUNS_ROOT:-${PROJECT_ROOT}/TESTRUNS}"
+SHOW_PROG="${SHOW_PROG:-xdg-open}"
+
+TEST_NAME="$(date +'%Y%m%d-%H%M%S')"
+TEST_ROOT="${TESTRUNS_ROOT}/${TEST_NAME}"
+DO_SHOW=no
+HAS_UNKNOWN_ARG=no
+
+
+function print_usage()
+{
+    cat <<EOF
+Usage: $(basename "${0}") [OPTS...]
+Where OPTS are of the following:
+    -h|--help    Print this message.
+    -s|--show    Auto-load the HTML test output on success.
+EOF
+}
+
+
+function update_symlink_latest()
+{
+    pushd "${TESTRUNS_ROOT}"
+    ln -fsT "${TEST_NAME}" latest
+    popd
+}
+
+
+function update_symlink_latest_success()
+{
+    pushd "${TESTRUNS_ROOT}"
+
+    [[ -h "latest_success" ]] && mv latest_success previous_success
+    ln -fsT "${TEST_NAME}" latest_success
+
+    popd
+}
+
+
+function remove_mapfiles()
+{
+    pushd "${TEST_ROOT}"
+
+    rm map-*-*
+
+    popd
+}
+
+
+function do_coverage_tests()
+{
+    pushd "${TEST_ROOT}"
+
+    # Clear coverage data. (Apparently *.gcda coverage files are generated
+    # alongside the binaries, which is sloppy.)
+    lcov --directory "${BUILD_ROOT}"/Coverage --zerocounters --rc lcov_branch_coverage=1
+
+    # Run tests
+    "${BUILD_ROOT}"/Coverage/test_measure
+
+    # Produce coverage webpages.
+    lcov --directory "${BUILD_ROOT}"/Coverage --capture --output-file "${TEST_ROOT}"/coverage.info --rc lcov_branch_coverage=1
+    lcov --remove "${TEST_ROOT}"/coverage.info '/usr/*' --output-file "${TEST_ROOT}"/coverage.info.cleaned --rc lcov_branch_coverage=1
+    genhtml -o "${TEST_ROOT}"/coverage "${TEST_ROOT}"/coverage.info.cleaned --rc lcov_branch_coverage=1
+
+    # Clear coverage data to have not disrupted contents of BUILD_ROOT/Coverage.
+    lcov --directory "${BUILD_ROOT}"/Coverage --zerocounters --rc lcov_branch_coverage=1
+
+    remove_mapfiles
+
+    popd 
+}
+
+
+function generate_toplevel_html()
+{
+    pushd "${TEST_ROOT}"
+
+    {
+	cat <<EOF
+        <!doctype html>
+        <html>
+        <head>
+        <title>CB Test Run ${TEST_NAME}</title>
+        </head>
+        <body>
+            CB Tests performed on ${TEST_NAME}.
+            <br>
+            <a href="coverage/index.html">Coverage Results</a>
+        </body>
+        </html>
+EOF
+    } >"${TEST_ROOT}/index.html"
+
+
+    popd
+}
+
+#Process commandline arguments
+while [[ $# -gt 0 ]]
+do
+    case "$1" in
+        -h|--help)
+            print_usage
+            exit 0
+            ;;
+        -s|--show)
+            DO_SHOW=yes
+            shift
+            ;;
+        *)
+            echo "Unknown argument \"$1\"." 1>&2
+            HAS_UNKNOWN_ARG=yes
+            ;;
+    esac
+    shift
+done
+
+if [[ "${HAS_UNKNOWN_ARG}" == "yes" ]]
+then
+    print_usage 1>&2
+    exit 1
+fi
+
+
+# Prepare directory for output of tests.
+[[ ! -d "${TESTRUNS_ROOT}" ]] && mkdir "${TESTRUNS_ROOT}"
+[[ ! -d "${TEST_ROOT}" ]] && mkdir "${TEST_ROOT}"
+
+update_symlink_latest
+
+# Perform tests.
+do_coverage_tests
+
+# Generate HTML entry point.
+generate_toplevel_html
+
+# Update symlinks
+update_symlink_latest_success
+
+# Show test report if requested.
+if [[ "${DO_SHOW}" == "yes" ]]
+then
+    "${SHOW_PROG}" "${TEST_ROOT}"/index.html
+fi
+
